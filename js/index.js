a7ffc1f7cfcffca5c59c7b04a6e173c170b8492f
jrevolorio2019058 <jrevolorio-2019058@kinal.edu.gt>
2024-01-25 01:46:49 -0600

    Agregando musica a cada página

---
+++ js/index.js
@@ -0,0 +1,73 @@
+let sound = new Audio;
+let contador = 0;
+let actual = 1.0;
+let cancion;
+
+
+function guardarCancion(char){
+
+    cancion = char;
+    contador = 1;
+    sound.pause();
+    sound = new Audio(cancion)
+    sound.play();
+
+}
+
+function reproducir(){
+
+    if(contador == 0){
+
+        sound = new Audio('./music/podcast.mp3');
+
+        sound.volume = actual;
+
+        sound.play();
+
+    }else{
+
+        cancion = reproducir
+
+        sound = new Audio(reproducir)
+
+        sound.volume = actual;
+
+        sound.play();
+
+
+    }
+
+
+}
+
+function pausar(){
+
+    sound.pause();
+
+}
+
+function bajarVolumen(){
+
+    if(actual > 0.1){
+
+        actual -= 0.1;
+        sound.volume = actual;
+
+    }else{
+        actual = 0;
+    }
+
+}
+
+function subirVolumen(){
+
+    if(actual < 0.9){
+
+        actual += 0.1;
+        sound.volume = actual;
+
+    }else{
+        actual = 1.0;
+    }
+
+}
